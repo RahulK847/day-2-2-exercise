a9565183202e087edaf6c9034c120c521b7f64a3
Rahulk847 <>
2021-06-03 16:31:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+height_square=float(height)**2
+BMI = float(weight)/height_square
+print(BMI)
+
+
+
+
+
+
+
+
+
